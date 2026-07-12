b54e96ea0f4dae52d8304edecaa61cabe45f1d3f
alex <aac521@nyu.edu>
2026-07-12 01:02:13 -0600

    Create Initial Project Structure

---
+++ verify_environment.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+
+print(f"NumPy:      {np.__version__}")
+print(f"Pandas:     {pd.__version__}")
+print(f"Matplotlib: {matplotlib.__version__}")
